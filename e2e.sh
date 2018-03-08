b958a9eaf76eeed45ef0fad22e889d3970022e69
Kakeru Ishii <i.kakeru@grimoire.gl>
2018-03-09 08:37:31 +0900

    fix: Try to encode base64

--- e2e.sh
+++ e2e.sh
@@ -1,6 +1,4 @@
 aws s3 cp register/ s3://$S3_BUCKET_URL/js/$CIRCLE_SHA1 --recursive --acl public-read --region ap-northeast-1
-pkgJson=`cat ./package.json`
-pkgJson=`echo ${pkgJson} | sed -e "s/[\r\n]\+//g"`
+pkgJson=`cat ./package.json | base64`
 pkgJson=`node  -e "console.log(encodeURIComponent(process.argv[1]))" -- $pkgJson`
-echo "$pkgJson"
 curl -X POST -d "repositoryURL=$CIRCLE_REPOSITORY_URL" -d "currentBranch=$CIRCLE_BRANCH" -d "currentBuildNumber=$CIRCLE_BUILD_NUM" -d "previousBuildNumber=$CIRCLE_PREVIOUS_BUILD_NUM" -d "sha1=$CIRCLE_SHA1" -d "pullRequest=$CI_PULL_REQUEST" -d "pkgJson=$pkgJson" $E2E_TRIGGER
